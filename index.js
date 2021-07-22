3c528104515b8277a5cb3a9f9b6b0b5f81d44734
Evan <evan.w.mclaughlin@gmail.com>
2021-07-22 11:00:19 -0400

    Add license

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+/**
+ Copyright (c) 2021-present, Eaton
+
+ All rights reserved.
+
+ This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
+ **/
 export * from './lightTheme';
 export * from './darkTheme';
